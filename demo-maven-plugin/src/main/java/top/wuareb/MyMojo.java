47c3925c89e6b035fcb58f751a305f50d78cbc53
wuxinjie1 <wuxinjie1@yiche.com>
2021-05-07 18:07:14 +0800

    Log AbstractMojo's attribute value

--- demo-maven-plugin/src/main/java/top/wuareb/MyMojo.java
+++ demo-maven-plugin/src/main/java/top/wuareb/MyMojo.java
@@ -20,62 +20,28 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Map;
 
 /**
  * Goal which touches a timestamp file.
  *
  * @goal touch
- * 
+ *
  * @phase process-sources
  */
-public class MyMojo
-    extends AbstractMojo
-{
+public class MyMojo extends AbstractMojo {
     /**
      * Location of the file.
-     * @parameter expression="${project.build.directory}"
+     * @parameter property="project.build.directory"
      * @required
      */
     private File outputDirectory;
 
-    public void execute()
-        throws MojoExecutionException
-    {
-        File f = outputDirectory;
-
-        if ( !f.exists() )
-        {
-            f.mkdirs();
+    public void execute() throws MojoExecutionException {
+        Map<?, ?> pluginContext = getPluginContext();
+        for (Map.Entry<?, ?> entry : pluginContext.entrySet()) {
+            getLog().info(String.format("key: [%s], value: [%s]", entry.getKey().toString(), entry.getValue().toString()));
         }
-
-        File touch = new File( f, "touch.txt" );
-
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
-
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
-                    w.close();
-                }
-                catch ( IOException e )
-                {
-                    // ignore
-                }
-            }
-        }
+        getLog().info(outputDirectory.toString());
     }
 }
