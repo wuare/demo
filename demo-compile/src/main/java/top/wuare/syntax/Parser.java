83f3293bc674b17ae2a56f9416c4a6b49f1e3087
wuare <wuareb@gmail.com>
2021-06-28 13:11:48 +0800

    Add parser demo

---
+++ demo-compile/src/main/java/top/wuare/syntax/Parser.java
@@ -0,0 +1,37 @@
+package top.wuare.syntax;
+
+/**
+ * parser
+ *
+ * @author wuare
+ * @date 2021/6/28
+ */
+public class Parser {
+
+    private final Scanner scanner = new Scanner();
+    private Scanner.Token curToken;
+
+    public Parser() {
+        scanner.init();
+        curToken = scanner.next();
+    }
+
+    public static void main(String[] args) {
+        Parser parser = new Parser();
+        parser.parsePackageStatement();
+    }
+
+    public void parsePackageStatement() {
+        match("package");
+        match("com.cxt.insurance.backend.controller.base");
+        match(";");
+    }
+
+    public void match(String val) {
+        if (val.equals(new String(curToken.getValue()))) {
+            curToken = scanner.next();
+            return;
+        }
+        throw new RuntimeException("syntax error");
+    }
+}
