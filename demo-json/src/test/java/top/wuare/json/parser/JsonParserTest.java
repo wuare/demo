16946b8d060125e7e1e7bb3e10662018d9ff7341
wuare <wuareb@gmail.com>
2021-07-23 15:19:32 +0800

    Add parser test case

--- demo-json/src/test/java/top/wuare/json/parser/JsonParserTest.java
+++ demo-json/src/test/java/top/wuare/json/parser/JsonParserTest.java
@@ -24,7 +24,7 @@ public class JsonParserTest {
 
     @Test
     public void testParser0() throws Exception {
-        File file = new File("E:/order.json");
+        File file = new File("Your Json File Path!!!");
         if (!file.exists()) {
             return;
         }
@@ -46,6 +46,54 @@ public class JsonParserTest {
         }
 
         String t = out.toString();
+        JsonParser parser = new JsonParser();
+        Object obj = parser.parse(t);
+        System.out.println(obj);
+    }
+
+    @Test
+    public void testParser1() {
+        String t = "{\n" +
+                "    \"transactionInfo\":{\n" +
+                "        \"transactionId\":\"\",\n" +
+                "        \"transactionType\":\"\",\n" +
+                "        \"transactionExeDate\":\"\",\n" +
+                "        \"transactionExeTime\":\"\"\n" +
+                "    },\n" +
+                "    \"channelInfo\":{\n" +
+                "        \"sellingChannelCode\":\"\",\n" +
+                "        \"clientSystemId\":\"\",\n" +
+                "        \"agency\":\"\",\n" +
+                "        \"agent\":\"\",\n" +
+                "        \"customerManager\":\"\"\n" +
+                "    },\n" +
+                "    \"response\":{\n" +
+                "        \"status\":{\n" +
+                "            \"statusCode\":\"400\",\n" +
+                "            \"statusMessage\":[\n" +
+                "\n" +
+                "            ]\n" +
+                "        },\n" +
+                "        \"quote\":{\n" +
+                "            \"amnt\":\"\",\n" +
+                "            \"prem\":\"\",\n" +
+                "            \"effectiveDate\":\"2015-01-01\",\n" +
+                "            \"expirationDate\":\"2016-01-01\",\n" +
+                "            \"components\":[\n" +
+                "                {\n" +
+                "                    \"riskCode\":\"RA001\",\n" +
+                "                    \"amnt\":\"100000\",\n" +
+                "                    \"prem\":\"10\"\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    \"riskCode\":\"RA002\",\n" +
+                "                    \"amnt\":\"200000\",\n" +
+                "                    \"prem\":\"20\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
         JsonParser parser = new JsonParser();
         Object obj = parser.parse(t);
         System.out.println(obj);
