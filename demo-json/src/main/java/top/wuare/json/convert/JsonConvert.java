d81172fc60be5b9f05c652fe688db5f31fd7cfb2
wuare <wuareb@gmail.com>
2021-09-21 21:43:55 +0800

    Remove unused method

--- demo-json/src/main/java/top/wuare/json/convert/JsonConvert.java
+++ demo-json/src/main/java/top/wuare/json/convert/JsonConvert.java
@@ -1,7 +1,5 @@
 package top.wuare.json.convert;
 
-import top.wuare.json.exception.CommonException;
-import top.wuare.json.lexer.Token;
 import top.wuare.json.parser.JsonParser;
 
 import java.beans.IntrospectionException;
@@ -22,84 +20,6 @@ public class JsonConvert {
 
     private final JsonParser jsonParser = new JsonParser();
 
-    @SuppressWarnings({"unchecked"})
-    public <T> T convertBean(String text, Class<T> t) {
-        Object object = jsonParser.parse(text);
-        if (object == null) {
-            return null;
-        }
-        if (t == Boolean.class && object instanceof Boolean) {
-            return (T) object;
-        }
-        if (t == String.class && object instanceof String) {
-            return (T) object;
-        }
-        if (object instanceof Map) {
-            Map<String, Object> map = (Map<String, Object>) object;
-            return mapToBean(t, map);
-
-        }
-        throw new CommonException("convert error");
-    }
-
-    @SuppressWarnings({"unchecked"})
-    private <T> T mapToBean(Class<T> t, Map<String, Object> map) {
-        T ins;
-        try {
-            ins = t.newInstance();
-            if (map.size() == 0) {
-                return ins;
-            }
-            Field[] fields = t.getDeclaredFields();
-            for (Field field : fields) {
-                String name = field.getName();
-                // TODO maybe Map or List, need handle
-                Object o = map.get(name);
-                if (o == null) {
-                    continue;
-                }
-                field.setAccessible(true);
-                if (o instanceof Map) {
-                    field.set(ins, mapToBean(field.getType(), (Map<String, Object>) o));
-                    continue;
-                }
-                fieldSetValue(field, ins, o);
-            }
-            return ins;
-        } catch (InstantiationException | IllegalAccessException e) {
-            throw new CommonException(e);
-        }
-    }
-
-    private void fieldSetValue(Field field, Object instance, Object o) throws IllegalAccessException {
-        Class<?> type = field.getType();
-        if (type == int.class || type == Integer.class) {
-            if (o instanceof BigDecimal) {
-                field.set(instance, ((BigDecimal) o).intValue());
-                return;
-            }
-        }
-        if (type == long.class || type == Long.class) {
-            if (o instanceof BigDecimal) {
-                field.set(instance, ((BigDecimal) o).longValue());
-                return;
-            }
-        }
-        if (type == float.class || type == Float.class) {
-            if (o instanceof BigDecimal) {
-                field.set(instance, ((BigDecimal) o).floatValue());
-                return;
-            }
-        }
-        if (type == double.class || type == Double.class) {
-            if (o instanceof BigDecimal) {
-                field.set(instance, ((BigDecimal) o).doubleValue());
-                return;
-            }
-        }
-        field.set(instance, o);
-    }
-
     public String toJson(Object obj) {
         if (obj == null) {
             return "null";
@@ -115,7 +35,7 @@ public class JsonConvert {
             return ((Boolean) obj).toString();
         }
         if (obj instanceof String) {
-            return "\"" + (String) obj + "\"";
+            return "\"" + obj + "\"";
         }
         if (obj instanceof List) {
             StringBuilder s0 = new StringBuilder("[");
--- demo-json/src/test/java/top/wuare/json/convert/JsonConvertTest.java
+++ demo-json/src/test/java/top/wuare/json/convert/JsonConvertTest.java
@@ -1,6 +1,5 @@
 package top.wuare.json.convert;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -13,21 +12,11 @@ public class JsonConvertTest {
 
     @Test
     public void testConvertBean() {
-        String text = "{ \"id\": 11, \"name\": \"North\" }";
-        JsonConvert convert = new JsonConvert();
-        UserTest userTest = convert.convertBean(text, UserTest.class);
-        Assert.assertEquals(11, userTest.getId());
-        Assert.assertEquals("North", userTest.getName());
+
     }
 
     @Test
     public void testConvertBean1() {
-        String text = "{ \"id\": 11, \"name\": \"North\", \"idCard\": { \"address\": \"Beijing\" } }";
-        JsonConvert convert = new JsonConvert();
-        UserTest userTest = convert.convertBean(text, UserTest.class);
-        Assert.assertEquals(11, userTest.getId());
-        Assert.assertEquals("North", userTest.getName());
-        Assert.assertNotNull(userTest.getIdCard());
-        Assert.assertEquals("Beijing", userTest.getIdCard().getAddress());
+
     }
 }
