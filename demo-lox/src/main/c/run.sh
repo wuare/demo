3a34aacafe0bb660c69167568a26632b6096ad4b
wuare <wuareb@gmail.com>
2022-02-21 19:07:08 +0800

    Create hash table

--- demo-lox/src/main/c/run.sh
+++ demo-lox/src/main/c/run.sh
@@ -1,2 +1,2 @@
 rm -f main
-gcc main.c chunk.c compiler.c debug.c memory.c object.c scanner.c value.c vm.c -o main
+gcc main.c chunk.c compiler.c debug.c memory.c object.c scanner.c table.c value.c vm.c -o main
